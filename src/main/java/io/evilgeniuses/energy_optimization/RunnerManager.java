a087966613a8042f954fd01724d3e029cfca27ee
Daniel Wunderer <wunderer.coding@gmail.com>
2022-07-21 10:10:01 +0200

    refactored Runner Class

--- src/main/java/io/evilgeniuses/energy_optimization/RunnerManager.java
+++ src/main/java/io/evilgeniuses/energy_optimization/RunnerManager.java
@@ -1,18 +1,22 @@
 package io.evilgeniuses.energy_optimization;
 
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RunnerManager {
 
-    private final CsvFileParser2 parser;
-    private final EnergyDataPointRepository repository;
-
-    public RunnerManager(CsvFileParser2 parser, EnergyDataPointRepository repository) {
-        this.parser = parser;
-        this.repository = repository;
+    @Bean
+    ApplicationRunner runner(JsonParser parser, VariableCostRepository repository) {
+        return args -> {
+            if (repository.findAll().isEmpty()) {
+                parser.parseAndSave();
+            }
+        };
     }
-
-    ApplicationRunner jsonRunner() {
+    @Bean
+    ApplicationRunner CsvRunner(CsvFileParser2 parser, EnergyDataPointRepository repository) {
         return args -> {
             if (repository.findAll().isEmpty()) {
                 parser.parseAndSave();
@@ -20,11 +24,4 @@ public class RunnerManager {
         };
     }
 
-    ApplicationRunner csvRunner() {
-        return args -> {
-            if (repository.findAll().isEmpty()) {
-                parser.parseAndSave();
-            }
-        };
-    }
 }
