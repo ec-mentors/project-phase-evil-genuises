c5d47aba59759f2245a97996475786fe929612bf
Daniel Wunderer <wunderer.coding@gmail.com>
2022-07-28 12:10:04 +0200

    refactored Forecast Manager class and deleted dummy TestClass

--- src/main/java/io/evilgeniuses/energy_optimization/ForecastManager.java
+++ src/main/java/io/evilgeniuses/energy_optimization/ForecastManager.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class ForecastManager {
     private final EnergyDataPointRepository repository;
@@ -15,32 +16,7 @@ public class ForecastManager {
         this.client = client;
     }
 
-    public List<EnergyDataPoint> getFutureData(String source){
-        var data = repository.findBySource(source);
-        var futurePrices = getFutureCosts();
-        List<EnergyDataPoint> futurePriceData = new ArrayList<>();
-
-
-        for (int i = 0; i < data.size(); i++) {
-            for (int j = 0; j < futurePrices.size(); j++) {
-                var hour = new DateTime(futurePrices.get(j).getEndTimeStamp()).hourOfDay().get();
-                var dayOfMonth = new DateTime(futurePrices.get(j).getEndTimeStamp()).dayOfMonth().get();
-                var month = new DateTime(futurePrices.get(j).getEndTimeStamp()).monthOfYear().get();
-                if (data.get(i).getEndTimeStamp().getHourOfDay() == hour
-                        && data.get(i).getEndTimeStamp().dayOfMonth().get() == dayOfMonth
-                        && data.get(i).getEndTimeStamp().monthOfYear().get() == month){
-                    var dataEntry = data.get(i);
-                    dataEntry.setPricePerKWH(futurePrices.get(j).getPricePerKWH());
-                    futurePriceData.add(dataEntry);
-                }
-
-            }
-
-        }
-        return futurePriceData;
-    }
-
-    public List<EnergyDataPoint> getFutureData2(String source) {
+    public List<EnergyDataPoint> getFutureData(String source) {
         var futurePrices = getFutureCosts();
         List<EnergyDataPoint> futurePriceData = new ArrayList<>();
 
@@ -59,10 +35,7 @@ public class ForecastManager {
         return futurePriceData;
     }
 
-
-
-
-     private List<VariableCost> getFutureCosts(){
+    private List<VariableCost> getFutureCosts() {
         var data = client.getDataFromFuture();
         var dataEntries = data.getData();
         List<VariableCost> energyList = new ArrayList<>();
